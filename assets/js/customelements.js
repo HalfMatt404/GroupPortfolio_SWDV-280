c10553ff1cc50e3f1058072ee3a6d67b0babee6e
Matthias <halfmat101@gmail.com>
2026-02-04 08:23:52 -0700

    Every page now uses the custom-header element, defined in customelements.js

--- assets/js/customelements.js
+++ assets/js/customelements.js
@@ -2,7 +2,34 @@ class CustomHeader extends HTMLElement {
     constructor() {
         super();
 
-        this.innerHTML = ``;
+        this.innerHTML = `<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">Home</a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavbar"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="projects.html"
+                                >Projects</a
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>`;
     }
 }
 
